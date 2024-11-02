d9945cca44efe81a9491ef46d203695b708899a7
Jackson Nace <jacksonnace964@gmail.com>
2024-11-02 17:42:18 -0500

    postgres is now connected

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,37 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const { Pool } = require('pg');
 const app = express();
-const port = 5000;
+// const port = 5000;
 
-app.get("/", (req, res) => {
-  res.json({
-    msg: "Hello World v2",
-  });
+const {PGHOST, PGDATABASE, PGUSER, PGPASSWORD} = process.env;
+console.log(PGHOST, PGDATABASE, PGUSER, PGPASSWORD);
+
+
+const pool = new Pool({ 
+  host: PGHOST,
+  database: PGDATABASE,
+  user: PGUSER,
+  password: PGPASSWORD,
+  port: 5432, // neons postgres port
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 15000, 
+  idleTimeoutMillis: 15000,
+})
+
+app.get("/", async (req, res) => {
+  try {
+    console.log("Attempting to connect to the database...");
+    const result = await pool.query("SELECT version(), current_setting('server_version'), current_setting('server_version_num')");
+    // const result = await pool.query("SELECT * FROM accounts");
+    console.log("Query successful, returning rows:", result.rows);
+    return res.json(result.rows); // Return data if query succeeds
+  } catch (errors) {
+    console.error("Error during database query:", errors);
+    return res.status(500).json("Database query failed"); // Provide a 500 status if query fails
+  } 
 });
 
-app.listen(port, () => {
-  console.log(`MockeyInterview Server is listening on port ${port}`);
-});
+app.listen(3001, console.log("Server Running"));
