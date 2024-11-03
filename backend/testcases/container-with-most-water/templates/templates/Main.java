9dd000ccfe8488004bf16e62d34f85b326492132
DedsecKnight <mikenguyen7268@gmail.com>
2024-11-02 20:31:25 -0500

    added container-with-most-water problem

---
+++ backend/testcases/container-with-most-water/templates/templates/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  private static int solve(int[] height) {
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    int res = solve(a);
+    System.out.println(res);
+    sc.close();
+  }
+}
---
+++ backend/testcases/container-with-most-water/templates/templates/template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(vector<int> &height) {}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  auto res = solve(a);
+  cout << res << '\n';
+}
