a54a90d365d09000c101e50e525026fc0fd50e42
DedsecKnight <mikenguyen7268@gmail.com>
2024-11-02 20:50:43 -0500

    added jump-game-ii problem

---
+++ backend/testcases/jump-game-ii/templates/template.py
@@ -0,0 +1,12 @@
+def solve(nums):
+  # TODO: Implement your solution here
+  pass
+
+def main():
+  n = int(input())
+  a = list(map(int, input().split()))
+  res = solve(a)
+  print(res)
+
+if __name__ == '__main__':
+  main()
