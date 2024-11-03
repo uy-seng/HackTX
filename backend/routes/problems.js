dc0613318df2f8f3006c3e45f9f8519d41f81cce
DedsecKnight <mikenguyen7268@gmail.com>
2024-11-02 23:46:57 -0500

    added endpoint to fetch solutions

--- backend/routes/problems.js
+++ backend/routes/problems.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getProblemById, getProblemIdByDifficulty } = require('../problems/manager');
+const { getProblemById, getProblemIdByDifficulty, getSolutionsByProblemId } = require('../problems/manager');
 
 router.get("/:level", async(req, res) => {
   const problemIds = getProblemIdByDifficulty(req.params.level);
   const problems = await Promise.all(problemIds.map((problemId) => getProblemById(problemId)));
   return res.json({ problems });
 });
+
+router.get("/:problemId/solutions", async(req, res) => {
+  const solutions = await getSolutionsByProblemId(req.params.problemId);
+  return res.json({
+    solutions
+  })
+})
 
 module.exports = router;
