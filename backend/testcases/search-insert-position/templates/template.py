7c987506779084252693b950900168352389544d
DedsecKnight <mikenguyen7268@gmail.com>
2024-11-03 06:28:31 -0600

    fixed template file

--- backend/testcases/search-insert-position/templates/template.py
+++ backend/testcases/search-insert-position/templates/template.py
@@ -7,7 +7,7 @@ def main():
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
     res = solve(a, k)
-    print(" ".join([str(c) for c in res]))
+    print(res)
 
 
 if __name__ == "__main__":
